fa9a445cfaf130032a5c6ce8f16fe883aa6609a6
Maxi <maxi.gatto96@gmail.com>
2023-05-30 21:50:33 -0300

    [FT] Validar QueryParams en el endpoint de listado de usuarios

--- login/src/routes/usersRoutes.ts
+++ login/src/routes/usersRoutes.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
 import { listUsers, authenticateUser, createUser } from '../controllers/userController';
-import { validateJWT, validateUserCredentials, validateAuthCredentials } from '../../../middlewares/index';
-
+import { validateJWT, validateUserCredentials, validateAuthCredentials, validatePaginationParams } from '../../../middlewares/index';
 
 
 const router = express.Router();
 
-router.get('/list', validateJWT, listUsers);
+router.get('/list', validateJWT, validatePaginationParams, listUsers);
 
 router.post('/register', validateUserCredentials, createUser);
 router.post('/auth', validateAuthCredentials, authenticateUser);
--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,5 +1,7 @@
 import validateUserCredentials from './validateUserCredentials';
 import validateAuthCredentials from './validateAuthCredentials';
+import validatePaginationParams from './validatePaginationParams';
+import validateReferer from './validateReferer';
 import validateJWT from './validateJWT';
 
-export { validateUserCredentials , validateAuthCredentials, validateJWT};
+export { validateUserCredentials , validateAuthCredentials, validatePaginationParams, validateReferer, validateJWT};
