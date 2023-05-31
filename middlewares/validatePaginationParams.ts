b89eb36922cb3552570dbf33ec072d8e8d17a5fd
Maxi <maxi.gatto96@gmail.com>
2023-05-30 22:33:23 -0300

    Quitadas comillas dobles

--- middlewares/validatePaginationParams.ts
+++ middlewares/validatePaginationParams.ts
@@ -2,9 +2,9 @@ const { query } = require('express-validator');
 import { validateFields } from './validateFields';
 
 const validatePaginationParams = [
-  query('page').optional().isInt().withMessage('"page" parameter must be a whole number'),
-  query('limit').optional().isInt().withMessage('"limit" parameter must be a whole number'),
-  query('email').optional().isString().withMessage('"email" parameter must be a string'),
+  query('page').optional().isInt().withMessage('page parameter must be a whole number'),
+  query('limit').optional().isInt().withMessage('limit parameter must be a whole number'),
+  query('email').optional().isString().withMessage('email parameter must be a string'),
   validateFields
 ];
 
